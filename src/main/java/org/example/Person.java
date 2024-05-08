0c2de3d003fcc3dd515200cd0d9d5bded9ea6cec
Maider Landaribar <maiderlandaribar@Maiders-MacBook-Pro.local>
2024-05-08 13:54:15 +0200

    tests de addItem

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -4,11 +4,19 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+// Clase abstracta Pack  , con atributos (category, maxCapacity, lista items)
+// clase hija Backpack hereda de Pack
+// Calse hija Bag hereda de Pack
+// Enum de categorias>> backpack, metal, ...
+// Person atributo > inventario List de Pack
+
 public class Person {
     private List<Item> backpack = new ArrayList<>();
+    private List<Item> metalBag = new ArrayList<>();
     public void addItem(Item item) {
-        backpack.add(item);
 
+        if(backpack.size() < 8) backpack.add(item);
+        else if(metalBag.size() < 4) metalBag.add(item);
     }
 
     public List<Item> getBackpack() { return this.backpack;
@@ -23,5 +31,5 @@ public class Person {
     public void organize() {
     }
 
-    public List<Item> getMetalBag() { return null;}
+    public List<Item> getMetalBag() { return this.metalBag;}
 }
--- src/test/java/org/example/PersonShould.java
+++ src/test/java/org/example/PersonShould.java
@@ -6,18 +6,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonShould {
     //-add item to backpack
+    // add until 8 to backpack
     //-add item to bag
+    // add until 4 to bag
+    // fill all packs
+
     //-volcar los items a otro sitio
     //-delete items
     //-organize (lanza hechizo)
 
     @Test
-    void AddOneItemItGoesToBackback() {
+    void AddOneItemItGoesToBackpack() {
         Person person = new Person();
         Item item = new Item("iron");
 
         person.addItem(item);
 
         assertEquals(1, person.getBackpack().size());
+    }
+    @Test
+    void DoNotAddMoreThanEightItemsToBackpack() {
+        Person person = new Person();
+        Item item = new Item("iron");
+        for (int i = 0; i < 10; i++) {
+            person.addItem(item);
+        }
+        assertEquals(8, person.getBackpack().size());
+    }
+    @Test
+    void AddOneItemItGoesToMetalBag() {
+        Person person = new Person();
+        Item item = new Item("iron");
+        for (int i = 0; i < 10; i++) {
+            person.addItem(item);
+        }
+        assertEquals(2, person.getMetalBag().size());
+    }
+    @Test
+    void DoNOtAddMoreThanFourItemsToMetalBAg() {
+        Person person = new Person();
+        Item item = new Item("iron");
+        for (int i = 0; i < 15; i++) {
+            person.addItem(item);
+        }
+        assertEquals(4, person.getMetalBag().size());
     }
 }
